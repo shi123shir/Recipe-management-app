7c6c44e7df8ddb12807db7ffc59f7715ce9ccd1a
shishir sharma <shishirsharma453@gmail.com>
2023-03-02 19:23:35 +0530

    fullapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import "./App.css";
+import ToggleTheme from "./components/toggle-theme";
+import Homepage from "./pages/homepage";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState(false);
+
+  console.log(theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setTheme,
+      }}
+    >
+      <div className={`App ${theme ? 'addToggleClass' : ''}`}>
+        <ToggleTheme />
+        <Homepage />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
